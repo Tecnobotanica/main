c94cfe3615d4445aab59244aa16f2a257f079fd0
Tecnobotanica <106548712+Tecnobotanica@users.noreply.github.com>
2022-06-15 02:48:25 +0200

    Update nodeMcu8266.ino

--- invernadero/arduino/nodeMcu8266.ino
+++ invernadero/arduino/nodeMcu8266.ino
@@ -1,21 +1,60 @@
-// Made by Jesus Tello @800869 in GitHub
-// Agradecimientos a GitHub Copilot por documentar el codigo linea por linea :D
+// Made by Jesus Tello @800869 in GitHub :D
 
-#include "SPI.h" // Libreria para la comunicacion SPI
+#include <Wire.h>
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+
+const char* ssid = "NodeMCU";       
+const char* password = "123456789"; 
+
+ESP8266WebServer server(80);
+
+int h=0;
+int t=0;
+int h2=0;
+int t2=0;
+int luminosidad=0;
+int humedadTierra=0;
+
+char c;
 
 void setup() {
-   digitalWrite(SS, HIGH);  
-   SPI.begin();
-   SPI.setClockDivider(SPI_CLOCK_DIV4);
+  Serial.begin(9600);
+  pinMode(D4, OUTPUT); 
+  WiFi.softAP(ssid, password);
+
+  IPAddress myIP = WiFi.softAPIP();
+  
+  server.begin();
 }
- 
+
 void loop() {
-   char c;
-   digitalWrite(SS, LOW); 
-   for (const char * p = "Prueba de comunicacionn\r" ; c = *p; p++) 
-   {
-      SPI.transfer(c);
-   }
-   digitalWrite(SS, HIGH); 
-   delay(2000);
+
+  while (Serial.available() > 0) {
+    c = Serial.read();
+
+    if(c == 'A'){
+      h = Serial.parseInt();
+    }
+    if(c == 'B'){
+      t = Serial.parseInt();
+    }
+    if(c == 'C'){
+      h2 = Serial.parseInt();
+    }
+    if(c == 'D'){
+      t2 = Serial.parseInt();
+    }
+    if(c == 'E'){
+      luminosidad = Serial.parseInt();
+    }
+    if(c == 'F'){
+      humedadTierra = Serial.parseInt();
+    }
+  }
+  
+/////////////////////////////////////
+   /*INCOMPLETO*/
+/////////////////////////////////////
 }
